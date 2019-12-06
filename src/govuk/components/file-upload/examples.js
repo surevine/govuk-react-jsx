bd346e4a998be41388817f02084aaecc1b814250
Andy Mantell <andy.mantell@surevine.com>
2019-12-06 12:01:40 +0000

    Fix bugs in Header and FileInput components

--- src/govuk/components/file-upload/examples.js
+++ src/govuk/components/file-upload/examples.js
@@ -2,4 +2,13 @@ import examples from '../../../../.cache/govuk-frontend-examples/file-upload.jso
 
 // Insert additional examples here if desired
 
-export default examples
+const modifiedExamples = Object.assign({}, examples)
+
+modifiedExamples.examples.forEach(function(item, index) {
+  // React gets upset if you try and set a value on a file input so we exclude this example
+  if (item.name === 'with value and attributes') {
+    delete modifiedExamples.examples[index]
+  }
+})
+
+export default modifiedExamples
--- src/govuk/components/header/header.story.js
+++ src/govuk/components/header/header.story.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { BrowserRouter } from 'react-router-dom'
 import { action } from '@storybook/addon-actions'
-import { Header } from '.'
+import { Header as BaseHeader } from '.'
 import examples from './examples'
 import processExampleData from '../../../../utils/processExampleData'
 
 const stories = storiesOf('header', module)
+
+const Header = function(props) {
+  return (
+    <BrowserRouter>
+      <BaseHeader {...props} />
+    </BrowserRouter>
+  )
+}
 
 for (const [index, example] of Object.entries(
   processExampleData(examples.examples)
