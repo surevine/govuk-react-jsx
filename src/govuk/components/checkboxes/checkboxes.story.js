8b999d42cf95648e9b7aedda52b39a6b450f958d
Andy Mantell <andy.mantell@surevine.com>
2020-01-15 13:15:18 +0000

    Add onChange handlers for the checkboxes demo

--- src/govuk/components/checkboxes/checkboxes.story.js
+++ src/govuk/components/checkboxes/checkboxes.story.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { Checkboxes } from './'
+import { Checkboxes as CheckboxesComponent } from './'
 import examples from './examples'
 import processExampleData from '../../../../utils/processExampleData'
 
 const stories = storiesOf('checkboxes', module)
 
+const Checkboxes = (props) => {
+  const initialValue = props.items.reduce((accumulator, item) => {
+      accumulator[item.value] = !!item.checked
+    return accumulator
+  }, {})
+
+  const [values, setValue] = useState(initialValue)
+
+  const onChangeHandler = (e) => {
+    setValue({
+      ...values,
+      [e.target.value]: !values[e.target.value]
+    })
+  }
+
+  props.items.forEach(item => {
+    item.onChange = onChangeHandler
+    item.checked = values[item.value]
+  })
+
+  return <CheckboxesComponent {...props} />
+}
+
 for (const [index, example] of Object.entries(
-  processExampleData(examples.examples)
+  processExampleData(examples.examples, 'checkboxes')
 )) {
-  stories.add(example.name, () => <Checkboxes {...example.data} />)
+  stories.add(example.name, () => {
+    return <Checkboxes {...example.data} />
+  })
 }
