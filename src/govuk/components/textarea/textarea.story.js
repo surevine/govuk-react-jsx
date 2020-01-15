bf37d18459c9aedcc192a818c429a723cb6ecce4
Andy Mantell <andy.mantell@surevine.com>
2020-01-15 12:08:39 +0000

    Add onChange handler to text area demo

--- src/govuk/components/textarea/textarea.story.js
+++ src/govuk/components/textarea/textarea.story.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { Textarea } from '.'
+import { Textarea as TextareaComponent } from '.'
 import examples from './examples'
 import processExampleData from '../../../../utils/processExampleData'
 
 const stories = storiesOf('textarea', module)
 
+const Textarea = (props) => {
+  const { value: initialValue, ...restProps } = props
+  const [value, setValue] = useState(initialValue)
+
+  const onChangeHandler = (e) => {
+    setValue(e.target.value)
+  }
+
+  return <TextareaComponent onChange={onChangeHandler} value={value} {...restProps} />
+}
+
 for (const [index, example] of Object.entries(
-  processExampleData(examples.examples)
+  processExampleData(examples.examples, 'textarea')
 )) {
-  stories.add(example.name, () => <Textarea {...example.data} />)
+  stories.add(example.name, () => {
+    return <Textarea {...example.data} />
+  })
 }
