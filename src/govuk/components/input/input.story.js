638e336961822f4f3a391309e92a86a3c0bb33cc
Andy Mantell <andy.mantell@surevine.com>
2020-02-10 11:31:51 +0000

    Forwarding ref into the Input component. Touches #2 #39

--- src/govuk/components/input/input.story.js
+++ src/govuk/components/input/input.story.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Input } from '.';
@@ -22,3 +22,32 @@ examples.examples.push({
 for (const example of Object.values(processExampleData(examples.examples))) {
   stories.add(example.name, () => <Input {...example.data} />);
 }
+
+function InputWithRef() {
+  const ref = React.createRef();
+
+  useEffect(() => {
+    console.log(ref);
+  });
+
+  return (
+    <>
+      <Input name="foo" ref={ref} label={{ children: 'With forwarded ref' }} />
+      <p className="govuk-body">
+        Additonal <code>ref</code> prop can be given which will be passed
+        through React.forwardRef and onto the input element.
+      </p>
+      <code>
+        <pre>
+          const ref = React.createRef();
+          <br />
+          return &lt;Input name="foo" ref=
+          {'{ref}'} /&gt;
+        </pre>
+      </code>
+      <p className="govuk-body">See devtools console for logged ref.</p>
+    </>
+  );
+}
+
+stories.add('Exposing ref', () => <InputWithRef />);
