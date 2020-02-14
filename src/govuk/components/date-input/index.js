da51f730a53d3a7279bc73f13af4a81e298350a6
usman.jamil@thoughtworks.com <usman.jamil@live.co.uk>
2020-02-14 12:06:17 +0000

    Made change to date-input to due to breaking tests

--- src/govuk/components/date-input/index.js
+++ src/govuk/components/date-input/index.js
@@ -74,7 +74,7 @@ function DateInput(props) {
         type="text"
         inputMode="numeric"
         pattern={item.pattern ? item.pattern : '[0-9]*'}
-        defaultValue={defaultValue ? defaultValue[item.name] : ''}
+        defaultValue={defaultValue ? defaultValue[item.name] : null}
       />
     </div>
   ));
