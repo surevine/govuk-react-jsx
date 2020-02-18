58fbf211729a285a02d4a2f8f2bca303664fa4d3
Andy Mantell <andy.mantell@surevine.com>
2020-02-18 10:17:09 +0000

    Make it easier to publish the package locally for development purposes

--- scripts/publish-local.sh
+++ scripts/publish-local.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 ./scripts/build.sh
+PACKAGE_VERSION=$(date +%s)
+sed "s/PACKAGE_VERSION/${PACKAGE_VERSION}.0.0/g" scripts/package.json > dist/package.json
 npm publish dist --registry http://localhost:4873/
