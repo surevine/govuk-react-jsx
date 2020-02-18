a0bc8c11eda9bb44a1aca35b8111d045efe80b3f
Andy Mantell <andy.mantell@surevine.com>
2020-02-18 11:01:35 +0000

    Map top level defaultValue prop on radios component through to defaultChecked on individual items
    Touches #48

--- src/govuk/components/radios/index.js
+++ src/govuk/components/radios/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Boolean } from '../../../utils/Boolean';
 
 function Radios(props) {
-  const { value, items, ...restProps } = props;
+  const { value, defaultValue, items, ...restProps } = props;
 
   // Map React-like `value` top level prop to the child items' checked status
   const processedItems = items.map(item => ({
     ...item,
     ...(value && { checked: item.value === value }),
+    ...(defaultValue && { defaultChecked: item.value === defaultValue }),
   }));
 
   return <Boolean items={processedItems} {...restProps} controlType="radios" />;
