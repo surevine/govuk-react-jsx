ab93e7c1d5c7521a3d18247b4df20602a637ebe3
Andy Mantell <134642+andymantell@users.noreply.github.com>
2020-11-23 10:54:53 +0000

    Hacky fix for missing hrefs in summary list examples

--- tests/govuk-frontend-diff.test.js
+++ tests/govuk-frontend-diff.test.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import processExampleData from '../utils/processExampleData';
-import * as components from '../src/govuk';
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import processExampleData from "../utils/processExampleData";
+import * as components from "../src/govuk";
 
-const glob = require('glob');
-const path = require('path');
-const ent = require('ent');
-const prettyhtml = require('@starptech/prettyhtml');
-const { HtmlDiffer } = require('@markedjs/html-differ');
-const diffLogger = require('@markedjs/html-differ/lib/logger');
+const glob = require("glob");
+const path = require("path");
+const ent = require("ent");
+const prettyhtml = require("@starptech/prettyhtml");
+const { HtmlDiffer } = require("@markedjs/html-differ");
+const diffLogger = require("@markedjs/html-differ/lib/logger");
 
 const govukFrontendPath = path.dirname(
-  require.resolve('govuk-frontend/package.json')
+  require.resolve("govuk-frontend/package.json")
 );
 
 function cleanHtml(dirtyHtml) {
@@ -22,18 +22,18 @@ function cleanHtml(dirtyHtml) {
 
 function hyphenatedToCamel(string) {
   return string
-    .split('-')
+    .split("-")
     .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
-    .join('');
+    .join("");
 }
 
 const htmlDiffer = new HtmlDiffer({
-  ignoreAttributes: ['style', 'src', 'disabled'],
+  ignoreAttributes: ["style", "src", "disabled"],
   ignoreSelfClosingSlash: true,
 });
 
 const allFixtures = glob
-  .sync(path.join(govukFrontendPath, 'govuk/components/**/fixtures.json'))
+  .sync(path.join(govukFrontendPath, "govuk/components/**/fixtures.json"))
   .map((file) => [path.basename(path.dirname(file)), require(file)]); // eslint-disable-line import/no-dynamic-require, global-require
 
 expect.extend({
@@ -45,18 +45,35 @@ expect.extend({
   },
 });
 
-describe('govuk-react-jsx output matches govuk-frontend', () => {
-  describe.each(allFixtures)('Component: %s', (component, fixtures) => {
+describe("govuk-react-jsx output matches govuk-frontend", () => {
+  describe.each(allFixtures)("Component: %s", (component, fixtures) => {
     const ReactComponent = components[hyphenatedToCamel(component)];
 
     it.each(
       fixtures.fixtures
         .filter((fixture) =>
           // Temporarily filter out tests that contain the word "undefined" until govuk-frontend@3.10.x lands
-          fixture.html ? fixture.html.indexOf('undefined') === -1 : true
+          fixture.html ? fixture.html.indexOf("undefined") === -1 : true
         )
         .map((fixture) => [fixture.name, fixture.options, fixture.html])
-    )('Example: %s', async (name, options, govukFrontendOutput) => {
+    )("Example: %s", async (name, options, govukFrontendOutput) => {
+      // Override values in specific fixtures to avoid issues
+      // Ideally follow up anything in here with a pull request to govuk-frontend resolving the issue
+      switch (`${component}:${name}`) {
+        // Fix href on a summary list actions example - href is required and should not be missing
+        case "summary-list:actions with classes":
+        case "summary-list:actions with html":
+          options.rows[0].actions.items[0].href = "hack";
+          govukFrontendOutput = govukFrontendOutput.replace(
+            'href=""',
+            'href="hack"'
+          );
+          break;
+
+        default:
+      }
+      // END overrides
+
       const props = processExampleData(options, component);
       const govukReactJsxOutput = ReactDOMServer.renderToStaticMarkup(
         <ReactComponent {...props} />
