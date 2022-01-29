5a7263fe469abddfa95c5f616f66272ff70d2c9f
Andy Mantell <134642+andymantell@users.noreply.github.com>
2022-01-29 14:24:56 +0000

    Set focus to skip link target to improve screen reader announcements
    See https://github.com/alphagov/govuk-frontend/pull/2450

--- src/govuk/components/skip-link/index.js
+++ src/govuk/components/skip-link/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function SkipLink(props) {
   const { href, className, children, ...attributes } = props;
+
+  const skipLinkRef = React.createRef();
+
+  useEffect(() => {
+    (async () => {
+      if (typeof document !== 'undefined') {
+        const { default: SkipLinkJS } = await import(
+          /* webpackChunkName: "govuk-frontend-skip-link" */
+          /* webpackMode: "lazy" */
+          /* webpackPrefetch: true */
+          'govuk-frontend/govuk/components/skip-link/skip-link'
+        );
+
+        if (skipLinkRef.current) {
+          new SkipLinkJS(skipLinkRef.current).init();
+        }
+      }
+    })();
+  }, [skipLinkRef]);
+
   return (
     <a
       href={href}
       className={`govuk-skip-link ${className || ''}`}
+      data-module="govuk-skip-link"
+      ref={skipLinkRef}
       {...attributes}
     >
       {children}
